4551b817e7fe974b83d2b097492a7f5b276ecfd1
vloe <vloe.dev@gmail.com>
2023-05-07 19:46:34 +0200

    auth work

--- apps/web/src/lib/trpc/routes/auth.ts
+++ apps/web/src/lib/trpc/routes/auth.ts
@@ -1,23 +1,33 @@
 import { t } from '$lib/trpc/t';
 import { z } from 'zod';
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+
+const SES = new SESClient({
+    apiVersion: '2010-12-01',
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+    }
+});
 
 const userSchema = z.object({
-	email: z
-		.string({ required_error: 'Email is required' })
-		.email({ message: 'Email must be valid' }),
-	password: z
-		.string({ required_error: 'Password is required' })
-		.trim()
-		.min(8, { message: 'Password must be at least 8 characters' })
-		.max(64, { message: 'Password must be less than 64 characters' })
-		.regex(/[a-z]/, { message: 'Password must contain a lowercase letter.' })
-		.regex(/[A-Z]/, { message: 'Password must contain an uppercase letter.' })
-		.regex(/[0-9]/, { message: 'Password must contain a number.' })
-		.regex(/^(?:(?!.*\s).)*$/, { message: 'Password must not contain whitespace.' })
+    email: z
+        .string({ required_error: 'Email is required' })
+        .email({ message: 'Email must be valid' }),
+    password: z
+        .string({ required_error: 'Password is required' })
+        .trim()
+        .min(8, { message: 'Password must be at least 8 characters' })
+        .max(64, { message: 'Password must be less than 64 characters' })
+        .regex(/[a-z]/, { message: 'Password must contain a lowercase letter.' })
+        .regex(/[A-Z]/, { message: 'Password must contain an uppercase letter.' })
+        .regex(/[0-9]/, { message: 'Password must contain a number.' })
+        .regex(/^(?:(?!.*\s).)*$/, { message: 'Password must not contain whitespace.' })
 });
 
 export const auth = t.router({
-	register: t.procedure.input(userSchema).query(({ input }) => {
-		return `hello ${input.email}`;
-	})
+    register: t.procedure.input(userSchema).query(async ({ input }) => {
+        return { success: true };
+    })
 });
--- apps/web/src/routes/register/+page.svelte
+++ apps/web/src/routes/register/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { trpc } from '$lib/trpc/client';
 	import { page } from '$app/stores';
+	import voulrWhiteLockup from '@voulr/assets/svgs/voulr-white-lockup.svg';
 
 	let email: string;
 	let password: string;
@@ -12,13 +13,48 @@
 </script>
 
 <div class="flex h-screen w-full flex-col items-center justify-center">
-	<p class="text-green-500">res: {$query.data}</p>
-	<p class="text-red-500">error: {$query.error?.message}</p>
-	<p class="text-white">email</p>
-	<input bind:value={email} />
-	<p class="text-white">password</p>
-	<input bind:value={password} />
-	<button class="bg-slate-400 text-white" on:click={async () => await $query.refetch()}>
-		click to register
-	</button>
+	<div class="flex w-[350px] max-w-[550px] flex-col justify-center gap-6 min-[700px]:w-1/2">
+		<img src={voulrWhiteLockup} alt="voulr" class="ml-5 w-24" />
+		<div class="flex w-full justify-center rounded-lg border border-neutral-800">
+			<form
+				class="flex w-11/12 flex-col items-center justify-center gap-6 py-12 text-white min-[700px]:w-3/4 min-[700px]:py-20"
+			>
+				<h1 class="w-full text-left text-2xl">Create your account</h1>
+				<label class="flex w-full flex-col gap-1.5">
+					<p class="pl-1.5">Email</p>
+					<input
+						bind:value={email}
+						class="h-12 w-full rounded-lg border border-neutral-800 bg-transparent pl-1.5 outline-none ring-voulr-blue transition-all duration-300 ease-in-out focus:ring-2"
+					/>
+					{#if $query.isError && $query.error.message.includes('Email')}
+						<p class="pl-1.5 text-sm text-red-500">
+							{$query.error.message}
+						</p>
+					{/if}
+				</label>
+				<label class="w-full">
+					<p class="p-1.5">Password</p>
+					<input
+						bind:value={password}
+						class="h-12 w-full rounded-lg border border-neutral-800 bg-transparent pl-1.5 outline-none ring-voulr-blue transition-all duration-300 ease-in-out focus:ring-2"
+					/>
+					{#if $query.isError && $query.error.message.includes('Password')}
+						<p class="pl-1.5 text-sm text-red-500">
+							{$query.error.message}
+						</p>
+					{/if}
+				</label>
+				<button
+					on:click={async () => await $query.refetch()}
+					class="h-12 w-full rounded-lg bg-voulr-pink"
+				>
+					Continue
+				</button>
+			</form>
+		</div>
+		<p class="ml-5 text-neutral-500">
+			Already have an account?
+			<a class="text-voulr-pink hover:brightness-75" href="/login">Sign In</a>
+		</p>
+	</div>
 </div>
