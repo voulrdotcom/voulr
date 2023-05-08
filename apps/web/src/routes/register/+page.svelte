72b7542c42c171bbf8dfb2c2d7c3f4c120f4c6d2
vloe <vloe.dev@gmail.com>
2023-05-08 17:34:46 +0200

    prisma setup

--- apps/web/src/routes/register/+page.svelte
+++ apps/web/src/routes/register/+page.svelte
@@ -17,14 +17,14 @@
 		<img src={voulrWhiteLockup} alt="voulr" class="ml-5 w-24" />
 		<div class="flex w-full justify-center rounded-lg border border-neutral-800">
 			<form
-				class="flex w-11/12 flex-col items-center justify-center gap-6 py-12 text-white min-[700px]:w-3/4 min-[700px]:py-20"
+				class="flex w-11/12 flex-col items-center justify-center gap-9 py-16 text-white min-[700px]:w-3/4 min-[700px]:py-20"
 			>
 				<h1 class="w-full text-left text-2xl">Create your account</h1>
 				<label class="flex w-full flex-col gap-1.5">
 					<p class="pl-1.5">Email</p>
 					<input
 						bind:value={email}
-						class="h-12 w-full rounded-lg border border-neutral-800 bg-transparent pl-1.5 outline-none ring-voulr-blue transition-all duration-300 ease-in-out focus:ring-2"
+						class="h-12 w-full rounded-lg border border-neutral-800 bg-transparent pl-3 outline-none ring-voulr-blue transition-all duration-300 ease-in-out focus:ring-2"
 					/>
 					{#if $query.isError && $query.error.message.includes('Email')}
 						<p class="pl-1.5 text-sm text-red-500">
@@ -32,11 +32,12 @@
 						</p>
 					{/if}
 				</label>
-				<label class="w-full">
-					<p class="p-1.5">Password</p>
+				<label class="flex w-full flex-col gap-1.5">
+					<p class="pl-1.5">Password</p>
 					<input
+						type="password"
 						bind:value={password}
-						class="h-12 w-full rounded-lg border border-neutral-800 bg-transparent pl-1.5 outline-none ring-voulr-blue transition-all duration-300 ease-in-out focus:ring-2"
+						class="h-12 w-full rounded-lg border border-neutral-800 bg-transparent pl-3 outline-none ring-voulr-blue transition-all duration-300 ease-in-out focus:ring-2"
 					/>
 					{#if $query.isError && $query.error.message.includes('Password')}
 						<p class="pl-1.5 text-sm text-red-500">
@@ -50,11 +51,12 @@
 				>
 					Continue
 				</button>
+
+				<p class="ml-5 text-neutral-500">
+					Already have an account?
+					<a class="text-voulr-pink hover:brightness-75" href="/login">Sign In</a>
+				</p>
 			</form>
 		</div>
-		<p class="ml-5 text-neutral-500">
-			Already have an account?
-			<a class="text-voulr-pink hover:brightness-75" href="/login">Sign In</a>
-		</p>
 	</div>
 </div>
