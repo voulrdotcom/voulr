ccd6c7508b88aa9d2129604e7674050eb0f116eb
vloe <vloe.dev@gmail.com>
2023-06-09 19:24:39 +0200

    cloudflare lets go

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,10 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
-
     kit: {
         adapter: adapter()
     }
