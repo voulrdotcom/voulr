d0716d0d9edc79a6a2fa08f866a0a28f4819b591
vloe <vloe.dev@gmail.com>
2023-06-09 19:40:00 +0200

    fix prod

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,10 +1,9 @@
 import type { Handle } from '@sveltejs/kit';
-import { publicRoutes } from '$lib/constants/publicRoutes';
 import { redirect } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-    if (!publicRoutes.includes(event.url.pathname)) {
-        throw redirect(303, event.locals.user ? '/' : '/login');
+    if (event.url.pathname === '/login' || event.url.pathname === '/register') {
+        throw redirect(303, '/');
     }
 
     const response = await resolve(event);
