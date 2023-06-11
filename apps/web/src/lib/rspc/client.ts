30e2286a5032c14c176faffcd15f19f5c317fe44
vloe <vloe.dev@gmail.com>
2023-06-11 13:17:21 +0200

    type error fix

--- apps/web/src/lib/rspc/client.ts
+++ apps/web/src/lib/rspc/client.ts
@@ -3,5 +3,5 @@ import type { Procedures } from '$lib/types/bindings';
 import { dev } from '$app/environment';
 
 export const client = createClient<Procedures>({
-    transport: new FetchTransport(`${dev ? 'http://localhost:8080' : 'https://voulr.com'}/rspc`)
+    transport: new FetchTransport(`${dev ? 'http://localhost:8080' : 'https://app.voulr.com'}/rspc`)
 });
